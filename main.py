f3bb2872133aaf045864dac7cddc774f74b71e08
aakashyadav27 <76722160+aakashyadav27@users.noreply.github.com>
2022-03-09 13:12:53 +0530

    Username mapping

--- main.py
+++ main.py
@@ -12,6 +12,37 @@ from functions import *
 app = Flask(__name__)
 data=pd.read_excel('planisware_data.xlsx')
 print(data.columns)
+NTID={"camachodj":"Cintron,Debra J","dentp":"Dent,Philip Damian"}
+
+def username_mapping(input_dicto):
+    new_dict=json.loads(input_dicto)
+    for k, v in new_dict.items():
+        print("yes")
+        if v == "camachodj":
+            new_dict[k] = "Cintron,Debra J"
+            print(new_dict)
+        elif v == "dentp":
+            new_dict[k] = "Dent,Philip Damian"
+        else:
+            pass
+    new_dict=json.dumps(new_dict)
+    return new_dict
+
+def ntid_mapping(ntid_dict):
+    #print(type(ntid_dict))
+    ntid_dictn = ntid_dict
+    for k, v in ntid_dictn .items():
+        if v == "Cintron,Debra J":
+            ntid_dictn [k] = "camachodj"
+            print(ntid_dictn)
+        elif v == "Dent,Philip Damian":
+            ntid_dictn[k] = "dentp"
+        else:
+            pass
+    #ntid_dictn = json.dumps(ntid_dictn)
+    return ntid_dictn
+
+
 @app.route("/", methods=['GET'])
 def hello_world():
     return render_template('index.html')
@@ -33,9 +64,11 @@ def GetOwnerList():
         else:
             no_filter_selected=pd.Series(data['Owner'].reset_index(drop='index').unique())
             no_filter_selected = no_filter_selected.to_json()
+            no_filter_selected = username_mapping(no_filter_selected)
             return  no_filter_selected
         output_data = pd.Series(output_data['Owner'].reset_index(drop='index').unique())
         output_data= output_data.to_json()
+        output_data = username_mapping(output_data)
         return output_data
     else:
         return 'Not found'
@@ -44,9 +77,14 @@ def GetOwnerList():
 def Projectnames():
     content_type1 = request.headers.get('Content-Type')
     category1 = request.json
-    new_data=data[data['Owner'] == category1['username']]
-    new_data= pd.Series(new_data['Name'].reset_index(drop='index').unique())
-    new_data = new_data.to_json()
+    if any(category1.values()):
+        category1=ntid_mapping(category1)
+        new_data=data[data['Owner'] == category1['username']]
+        new_data= pd.Series(new_data['Name'].reset_index(drop='index').unique())
+        new_data = new_data.to_json()
+    else:
+        new_data = pd.Series(data['Name'].reset_index(drop='index').unique())
+        new_data = new_data.to_json()
     return new_data
 
 """
